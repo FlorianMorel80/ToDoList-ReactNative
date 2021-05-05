887023c8595ace2f5898d9b3011117a2b61291a7
Your Name <morel.florian80@hotmail.fr>
2021-05-05 10:53:13 +0200

    header ok

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// Platform sert à pouvoir faire un choix de la plateforme que l'on va selectionner pour effectuer telle action ou modification 
+import { StyleSheet, SafeAreaView, Platform, View } from 'react-native';
+
+//------------Import components-------------------
+import Header from './src/components/_Shared/Header';
+//------------------------------------------------
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // SafeAreaView permet d'éviter que nos éléments aillents trop haut sur le mobile, fonctionne que pour IOS
+    <SafeAreaView style={styles.container}>
+      <Header />
+    </SafeAreaView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // Ici je dis, sur la platform android tu vas rajouter un padding top de 25
+    // Le 25 est déjà appliqué de base donc je mets en commentaire 
+
+    // paddingTop: Platform.OS === 'android' ? 25 : 0,
+    paddingLeft: 20,
+    paddingRight: 20
   },
 });
